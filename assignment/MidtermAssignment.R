7f1c460ba95bda54e2595607d3804b30b067a0aa
Suppawit Wiriyapuwadol <suppawit.41218@gmail.com>
2021-10-16 18:45:16 +0700

    packages

--- assignment/MidtermAssignment.R
+++ assignment/MidtermAssignment.R
@@ -2,3 +2,12 @@
 
 UdemyCSV <- read.csv("https://raw.githubusercontent.com/sit-2021-int214/031-Finance-Accounting-Courses---Udemy-13K-course-/main/assignment/data/udemy_output_All_Finance__Accounting_p1_p626.csv")
 View(UdemyCSV)
+
+install.packages("readr")
+install.packages("assertive")
+install.packages("stringr")
+install.packages("dplyr")
+library("readr")
+library("assertive")
+library("stringr")
+library("dplyr")
