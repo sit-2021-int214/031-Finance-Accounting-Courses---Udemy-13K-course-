6490a22b8631ebb86fb059ef12657b1b4ea259e0
อภิสิทธื์ ลอมโฮม <frongrider2@gmail.com>
2021-10-16 18:33:30 +0700

    add csv

---
+++ assignment/MidtermAssignment.R
@@ -0,0 +1,4 @@
+# import csv
+
+UdemyCSV <- read.csv("https://raw.githubusercontent.com/sit-2021-int214/031-Finance-Accounting-Courses---Udemy-13K-course-/main/assignment/data/udemy_output_All_Finance__Accounting_p1_p626.csv")
+View(UdemyCSV)
