9388a20d8254760af01731b4a934eea392bdbb67
Suppawit Wiriyapuwadol <suppawit.41218@gmail.com>
2021-10-16 16:18:16 +0700

    01

--- assignment/WS01_63130500111/WS01_63130500111.R
+++
@@ -1,6 +0,0 @@
-#Loading dataset from SAT_original.csv and assign variable named sat_score
-sat_score <- read.csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/SAT_original.csv")
-
-glimpse(sat_score)
-
-
