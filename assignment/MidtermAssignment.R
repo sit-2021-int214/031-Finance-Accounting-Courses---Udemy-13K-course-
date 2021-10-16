5ead57ba5e3794921650efb7d4c5b91e633c3d3f
sahathat <sahatat44@gmail.com>
2021-10-16 19:43:37 +0700

    v.data has clearing
    clear a value of discount not found or na value and change format price_string of character

--- assignment/MidtermAssignment.R
+++ assignment/MidtermAssignment.R
@@ -11,3 +11,23 @@ library("readr")
 library("assertive")
 library("stringr")
 library("dplyr")
+
+# Data cleaning
+
+# check data type of Udemy
+glimpse(Udemy)
+
+# by id is duplicate
+Udemy$id %>% duplicated() %>% sum()
+# 0
+# Umedy have some data are not duplicate
+
+# When amount price of course aren't show in table should replace by o
+Udemy$discount_price__amount <- replace(Udemy$discount_price__amount,is.na(Udemy$discount_price__amount), 0)
+
+# When currency price of course aren't show in table should replace by INR
+Udemy$discount_price__currency <- replace(Udemy$discount_price__currency,Udemy$discount_price__currency=="", "INR")
+
+# and replace by string with ₹+amount with mutate
+Udemy <- Udemy %>% mutate(discount_price__price_string=paste("₹",discount_price__amount)) # convert to character
+# note your can't use character plus character by character + character but can use paste(x1,x2,...,(collapse="-")[optional]) 
