a709ce80c512e62b9b06a24c4d0cde7173802ff7
sahathat <sahatat44@gmail.com>
2021-10-21 20:21:22 +0700

    v.fix code

--- assignment/MidtermAssignment.R
+++ assignment/MidtermAssignment.R
@@ -12,6 +12,35 @@ library("assertive")
 library("stringr")
 library("dplyr")
 
+# Explore data 
+glimpse(Udemy) #data of Udemy have 13608 rows and 20 columns by each columns have
+
+# id : The course ID of that particular course.
+# title : Shows the unique names of the courses available under the development category on Udemy.
+# url: Gives the URL of the course.
+# is_paid : Returns a boolean value displaying true if the course is paid and false if otherwise.
+
+# num_subscribers : Shows the number of people who have subscribed that course.
+# avg_rating : Shows the average rating of the course.
+# avg rating recent : Reflects the recent changes in the average rating.
+# rating : Reflects the recent changes in the average rating.
+# num_reviews : Gives us an idea related to the number of ratings that a course has received.
+# is_wishlisted : Show course who are favorite by marked wishlist.
+
+# num_ published_lectures : Shows the number of lectures the course offers.
+# num_ published_ practice_tests : Gives an idea of the number of practice tests that a course offers.
+
+# created : The time of creation of the course.
+# published_time : Time of publishing the course.
+
+# discounted_ price_amount : The discounted price which a certain course is being offered at.
+# discounted_ price_currency : The currency corresponding to the discounted price which a certain course is being offered at.
+# discounted_ price_price_string :
+
+# price_ detail_amount : The original price of a particular course.
+# price_ detail_currency : The currency corresponding to the price detail amount for a course.
+# price_ detail_price_string : 
+
 # Data cleaning
 
 # check data type of Udemy
@@ -40,12 +69,12 @@ Udemy$discount_price__currency <- replace(Udemy$discount_price__currency,Udemy$d
 # detail of price
 Udemy$price_detail__amount <- replace(Udemy$price_detail__amount,Udemy$price_detail__amount=="", "INR")
 
-# and replace by string with ₹+amount with mutate
+# and replace by string with current+amount with mutate
 # discount price
-Udemy <- Udemy %>% mutate(discount_price__price_string=paste("₹",discount_price__amount))
+Udemy <- Udemy %>% mutate(discount_price__price_string=paste(discount_price__currency,discount_price__amount))
 # note your can't use character plus character by character + character but can use paste(x1,x2,...,(collapse="-")[optional])
 # price detail
-Udemy <- Udemy %>% mutate(price_detail__price_string=paste("₹",price_detail__amount))
+Udemy <- Udemy %>% mutate(price_detail__price_string=paste(price_detail__currency,price_detail__amount))
 
 # find title name,rating and number of reviews who are top of 10 courses have the most reviews in each course
 Udemy %>%
